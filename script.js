aaaf6602cb6baecb67ccfcbc24648b407851e8e5
phurin prasit <phurin@web1.dara.ac.th>
2021-07-25 22:38:10 +0700

    v1

--- script.js
+++ script.js
@@ -1,1 +1,57 @@
-/* Your code here */
+const cout = console.log
+let currentInput = ''
+
+const input = document.querySelector('input')
+const add = document.querySelector('#add')
+const coms = document.querySelector('#coms')
+const list = document.querySelector('#list')
+
+const addinput = () => {
+    if (input.value == "") {
+        alert("task is empty")
+    }
+    else {
+        const btn = document.createElement('button')
+        btn.setAttribute('class', 'bg-green-100')
+        btn.innerHTML = "Done"
+        btn.addEventListener("click", function(event){
+            h1.style.textDecoration = "line-through"
+            h1.remove()
+            coms.insertBefore(h1, coms.firstChild)
+
+        })
+        const btnDel = document.createElement('button')
+        btnDel.setAttribute('class', 'bg-red-100')
+        btnDel.innerHTML = "Delete"
+        btnDel.addEventListener("click", function(event){
+            h1.remove()
+        })
+        const h1 = document.createElement('h1')
+        h1.innerHTML = input.value
+        btn.style.visibility = "hidden"
+        btnDel.style.visibility = "hidden"
+        h1.addEventListener("mouseenter", function (event){
+            if(h1.style.textDecoration !== "line-through"){
+                btn.style.visibility = "visible"
+            btnDel.style.visibility = "visible"
+            }
+        })
+        h1.addEventListener("mouseleave", function (event){
+            btn.style.visibility = "hidden"
+            btnDel.style.visibility = "hidden"
+        })
+        h1.append(btn)
+        h1.append(btnDel)
+        input.value = ""
+        list.insertBefore(h1, list.firstChild)
+    }
+
+}
+
+input.addEventListener("keyup", function (event) {
+    if (event.key === 'Enter') {
+        event.preventDefault();
+        add.click();
+    }
+});
+
